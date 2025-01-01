682d05690e1d4cfc3d1ca831dbd2c23443aa554c
EugeneGpil <gpilligrim@gmail.com>
2025-01-01 15:58:03 +0700

    1

---
+++ docker/containers/php/set_perm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# $1 user
+# $2 folder
+set_shared_permissions()
+{
+  	printf "For files in folder %s give permission to write for users %s and www-data\n" "$2" "$1"
+    setfacl -Rm u:www-data:rwX,u:"$1":rwX "$2"
+
+    printf "For all future files in folder %s give permission to write for users %s и www-data\n" "$2" "$1"
+    setfacl -Rdm u:www-data:rwx,u:"$1":rwx "$2"
+}
+
+set -e
+
+printf "Make owner of project user %s\n" "$1"
+chown -R "$1:$1" ./
+
+printf "Give permission for writing only to %s for all project\n" "$1"
+find /var/www/back -type f -exec chmod 644 {} \;
+find /var/www/back -type d -exec chmod 755 {} \;
+
+set_shared_permissions "$1" "/var/www/back/bootstrap/cache"
+set_shared_permissions "$1" "/var/www/back/storage"
+
+printf "Add execution for bin files\n"
+chmod u+x /var/www/back/vendor/bin/* || true
+chmod u+x /var/www/back/node_modules/.bin/* || true

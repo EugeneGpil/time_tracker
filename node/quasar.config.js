2d9f2d9fa7d95ee5cb57f355bd39c38547658e8b
EugeneGpil <gpilligrim@gmail.com>
2025-01-05 21:59:39 +0700

    1

--- node/quasar.config.js
+++ node/quasar.config.js
@@ -72,7 +72,11 @@ export default defineConfig((ctx) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
       // https: true,
-      open: true, // opens browser window automatically
+      open: false, // opens browser window automatically
+      client: {
+        // webSocketURL: 'ws://time-tracker.lc'
+        webSocketUrl: `ws://${ctx.APP_URL}`
+      }
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework

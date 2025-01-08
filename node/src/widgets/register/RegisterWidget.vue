3636f69130f01217058355ed19a71f36a6a8ff62
EugeneGpil <gpilligrim@gmail.com>
2025-01-08 19:28:21 +0700

    1

--- node/src/widgets/register/RegisterWidget.vue
+++ node/src/widgets/register/RegisterWidget.vue
@@ -3,6 +3,11 @@
     <div class="register-widget__logo">
       <LogoComponent />
     </div>
+    <div class="register-widget__main-text-container">
+      <div class="register-widget__main-text">
+        Register a new membership
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,9 +25,9 @@ export default {
 @use 'src/css/color_utils.scss' as *;
 
 .register-widget {
-  width: min(90%, 360px);
+  width: min(90%, 22.5rem);
   @include color(background-color, block-background);
-  border-top-width: 3px;
+  border-top-width: 0.188rem;
   border-top-style: solid;
   @include color(border-top-color, primary);
   border-top-left-radius: 0.25rem;
@@ -32,6 +37,17 @@ export default {
     padding-top: 0.7rem;
     padding-bottom: 0.7rem;
     overflow-y: hidden;
+    border-bottom-width: 0.063rem;
+    border-bottom-style: solid;
+    @include color(border-bottom-color, between-blocks-border);
+  }
+
+  &__main-text-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 1.25rem;
+    padding-bottom: 1.25rem;
   }
 }
 </style>

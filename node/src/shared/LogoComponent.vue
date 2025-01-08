44505d63c8e5382e4f1a3b0d3b49c97361c81bb5
EugeneGpil <gpilligrim@gmail.com>
2025-01-08 18:42:30 +0700

    1

--- node/src/shared/LogoComponent.vue
+++ node/src/shared/LogoComponent.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>TIME TRACKER</div>
+  <div class="logo">
+    <span>TIME</span>
+    <br>
+    <span>TRACKER</span>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.logo {
+  font-family: Sixtyfour;
+  font-size: min(2.1rem, 10vw);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  line-height: min(2.3rem, 11vw);
+}
+</style>
--- node/src/widgets/register/RegisterWidget.vue
+++ node/src/widgets/register/RegisterWidget.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="register-widget">register widget</div>
+  <div class="register-widget">
+    <div class="register-widget__logo">
+      <LogoComponent />
+    </div>
+  </div>
 </template>
+
+<script>
+import LogoComponent from 'src/shared/LogoComponent.vue';
+
+export default {
+  components: {
+    LogoComponent,
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 @use 'src/css/color_utils.scss' as *;
+
 .register-widget {
-  @include color(color, primary);
+  width: min(90%, 360px);
   @include color(background-color, block-background);
+  border-top-width: 3px;
+  border-top-style: solid;
+  @include color(border-top-color, primary);
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+
+  &__logo {
+    padding-top: 0.7rem;
+    padding-bottom: 0.7rem;
+    overflow-y: hidden;
+  }
 }
 </style>

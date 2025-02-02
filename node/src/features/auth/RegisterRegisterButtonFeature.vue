75bf3aef760b5bcfab8be57d754eee986bd1b35d
EugeneGpil <gpilligrim@gmail.com>
2025-02-03 05:37:24 +0700

    1

--- node/src/features/auth/RegisterRegisterButtonFeature.vue
+++ node/src/features/auth/RegisterRegisterButtonFeature.vue
@@ -1,16 +1,22 @@
 <template>
   <ButtonComponent
-    :disabled="false"
+    :disabled="registerStore.isRegisteringInProgress"
     text="Register"
+    @click="registerStore.register()"
   />
 </template>
 
 <script>
 import ButtonComponent from "src/shared/ButtonComponent.vue";
+import {useRegisterStore} from 'stores/register/register.js';
+import {mapStores} from 'pinia';
 
 export default {
+  computed: {
+    ...mapStores(useRegisterStore),
+  },
   components: {
-    ButtonComponent
+    ButtonComponent,
   }
 }
 </script>
--- node/src/stores/register/registerAction.js
+++ node/src/stores/register/registerAction.js
@@ -1,30 +1,34 @@
-export default async () => {
+export default async function () {
   if (this.isRegisteringInProgress === true) {
     return;
   }
 
   this.isRegisteringInProgress = true;
 
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      name: this.name,
-      email: this.email,
-      password: this.password,
-      password_confirmation: this.retypePassword,
-    }),
-  }
+  setTimeout(() => {
+    this.isRegisteringInProgress = false;
+  }, 3000)
 
-  const response = await fetch(
-    'http://api.time-tracker.lc/v1/register',
-    options,
-  )
-
-  console.log(response)
-
-  this.isRegisteringInProgress = false;
+  // const options = {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Accept': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     name: this.name,
+  //     email: this.email,
+  //     password: this.password,
+  //     password_confirmation: this.retypePassword,
+  //   }),
+  // }
+  //
+  // const response = await fetch(
+  //   'http://api.time-tracker.lc/v1/register',
+  //   options,
+  // )
+  //
+  // console.log(response)
+  //
+  // this.isRegisteringInProgress = false;
 }

d32e221b185721109447e39952db9211d62575c8
EugeneGpil <gpilligrim@gmail.com>
2025-02-03 05:09:29 +0700

    1

--- node/src/shared/ButtonComponent.vue
+++ node/src/shared/ButtonComponent.vue
@@ -24,6 +24,7 @@ export default {
   border-radius: 0.25rem;
   padding: min(0.375rem, 1.25vw) min(0.75rem, 2.5vw);
   @include color(background-color, primary);
+  @include color(color, white);
 
   &:hover {
     @include color(background-color, primary-darker);
